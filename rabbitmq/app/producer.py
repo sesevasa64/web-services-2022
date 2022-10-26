4e474a4d7ec8c4ee00cb76c08b91db4ffe628044
Vsevolod <sesevasa64@gmail.com>
2022-10-26 18:50:51 +0500

    changed connection to channel argument in producer

--- rabbitmq/app/producer.py
+++ rabbitmq/app/producer.py
@@ -3,6 +3,7 @@ import json
 import time
 import requests
 from datetime import datetime, timedelta
+from pika.adapters.blocking_connection import BlockingChannel
 from .settings import CURRENCY_LIST, API_KEY_PATH, DELAY
 from .utils import create_logger
 
@@ -27,8 +28,7 @@ def headers():
     return {"apikey": read_key()}
 
 
-def produce(connection: pika.BlockingConnection):
-    channel = connection.channel()
+def produce(channel: BlockingChannel):
     response = requests.get(API_URL, params=API_PARAMS, headers=headers())
     content = json.loads(response.text)
     rates = content["rates"]
@@ -41,12 +41,13 @@ def produce(connection: pika.BlockingConnection):
 def main():
     con_params = pika.ConnectionParameters('localhost')
     connection = pika.BlockingConnection(con_params)
+    channel = connection.channel()
     t = datetime.strptime(DELAY, "%H:%M:%S")
     dt = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
     logger.debug("Producer service started")
     try:
         while True:
-            produce(connection)
+            produce(channel)
             time.sleep(dt.total_seconds())
     except KeyboardInterrupt:
         pass
